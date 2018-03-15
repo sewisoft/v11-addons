bba2298d01b4d0a38c3e49fbc1d13fa45f7e3e76
jesni <jesni@cybrosys.in>
2018-03-15 15:50:43 +0530

    [CHG] Updated module according to the version 1.0.9 from V10

---
+++ eq_base/RELEASE_NOTES.md
@@ -0,0 +1,11 @@
+## Modul eq_base
+
+#### 15.03.2018
+#### Version 1.0.1
+##### CHG
+- Updated module according to the version '1.0.9' from V10.
+
+#### 23.11.2017
+#### Version 1.0.0
+##### ADD
+- Basic fields for more EQ Modules
--- eq_base/__manifest__.py
+++ eq_base/__manifest__.py
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2017-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
     'name': 'Equitania Base',
     'license': 'AGPL-3',
-    'version': '11.0.1.0.0',
+    'version': '1.0.1',
     'description': """Basic fields for more EQ Modules""",
     'author': 'Equitania Software GmbH',
     'website': 'www.myodoo.de',
