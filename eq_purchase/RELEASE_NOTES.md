47c509e7fdbb092b2b79001404dca304513a055c
jesni <jesni@cybrosys.in>
2018-03-17 12:56:50 +0530

    [CHG] Updated module according to the version 1.0.31 from V10.

---
+++ eq_purchase/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Modul eq_purchase
+
+#### 17.03.2018
+#### Version 1.0.5
+##### CHG
+- Updated module according to the version '1.0.31' from V10.
+
--- eq_purchase/models/report_purchase_order.py
+++ eq_purchase/models/report_purchase_order.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2017-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import datetime
 from odoo import models, api
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as OE_DFORMAT
 
+# Funktionen für Model, die über Report aufgerufen werden können
 
 class ReportPurchaseOrder(models.Model):
     _inherit = 'purchase.order'
@@ -81,3 +102,50 @@ class ReportPurchaseOrderLine(models.Model):
         :return:
         """
         return self.env["eq_report_helper"].get_qty(value, language, 'Purchase Unit of Measure Report [eq_purchase]')
+
+    def get_delivery_date_flag(self):
+        """
+        Anzeige geplantes Datum auf Basis der Checkbox 'Das geplante Datum in der Bestellung anzeigen [eq_purchase]' in der Konfiguration
+        :param value:
+        :param language:
+        :return:
+        """
+        ir_values_obj = self.env['ir.values']
+        show_delivery_date = ir_values_obj.get_default('purchase.order', 'show_planned_date')
+        return show_delivery_date
+
+    def get_delivery_date_kw_flag(self):
+        """
+        Anzeige geplante Datum auf Basis der Checkbox 'Das geplante Datum in der Bestellung anzeigen [eq_purchase] ' in der Konfiguration
+        :param value:
+        :param language:
+        :return:
+        """
+        ir_values_obj = self.env['ir.values']
+        show_calendar_week = ir_values_obj.get_default('purchase.order', 'use_calendar_week')
+        return show_calendar_week
+
+    def get_delivery_date_kw(self, delivery_date):
+        """
+        Anzeige geplante Datum als KW
+        :param value:
+        :param language:
+        :return:
+        """
+        if delivery_date != False:
+            new_date = delivery_date.split(' ')
+            date = datetime.datetime.strptime(new_date[0], "%Y-%m-%d")
+            year = date.year
+            month = date.month
+            day = date.day
+            d = datetime.date(year, month, day)
+            kw = d.isocalendar()[1]
+            kw = str(kw)
+
+            return kw
+
+    def get_formatted_planned_date(self, planned_date):
+        planned_date = planned_date.split(" ")
+        planned_date = datetime.datetime.strptime(planned_date[0], OE_DFORMAT)
+        planned_date = planned_date.strftime('%d.%m.%Y')
+        return planned_date
