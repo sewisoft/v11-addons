16a5584e601ec6b38bec03dd10fe8337fa6d842a
jesni <jesni@cybrosys.in>
2018-03-23 10:50:05 +0530

    [CHG] Updated module according to the version 1.0.5 from V10.

---
+++ eq_pricelist/RELEASE_NOTES.md
@@ -0,0 +1,14 @@
+## Modul eq_pricelist
+
+#### 23.03.2018
+#### Version 1.0.1
+##### CHG
+- Updated module according to the version '1.0.5' from V10.
+
+
+#### 16.06.2017
+#### Version 1.0.0
+##### ADD
+- Neues Modul für die Anpassungen der Preislisten
+- Preislistensuche
+
--- eq_pricelist/__manifest__.py
+++ eq_pricelist/__manifest__.py
@@ -1,16 +1,36 @@
 # -*- coding: utf-8 -*-
-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2017-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
     'name': "Equitania Pricelist",
     'license': 'AGPL-3',
-    'version': '11.0.1.0.0',
+    'version': '1.0.1',
     'category': 'pricelist',
     'description': """ Extension for pricelist-positions """,
     'author': 'Equitania Software GmbH',
     'summary': 'Pricelist Extension',
     'website': 'www.myodoo.de',
     "depends": ['base', 'base_setup', 'product', 'sale'],
-    'data': ['views/eq_pricelist_view.xml'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/eq_pricelist_view.xml'],
     "active": False,
     "installable": True
 }
